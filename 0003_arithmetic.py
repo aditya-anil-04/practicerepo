4795dcd9ce62f239925a48ba849a3a39e190cb62
Aditya Anil <127827576+aditya-anil-04@users.noreply.github.com>
2026-07-06 19:52:14 -0400

    Add files via upload

---
+++ 0003_arithmetic.py
@@ -0,0 +1,13 @@
+# Concept: Arithmetic Operations
+
+a = 10
+b = 7
+
+print("a = ", a)
+print("b = ", b)
+
+print("a + b = ", a + b)
+print("a - b = ", a - b)
+print("a * b = ", a * b)
+print("a / b = ", a / b)
+print("a % b = ", a % b)
